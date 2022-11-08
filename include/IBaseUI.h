bb7fdba43994bf27934d75d5031d917a448e1010
Bien Pham <pham.bien10@gmail.com>
2022-11-08 08:29:13 +0800

    Update xash exclusive IBaseUI

--- include/IBaseUI.h
+++ include/IBaseUI.h
@@ -27,13 +27,15 @@ from your version.
 
 #include "interface.h"
 
-class IEngineSurface;
-
 class IBaseUI : public IBaseInterface
 {
 public:
 	virtual void Initialize( CreateInterfaceFn *factories, int count ) = 0;
-	virtual void Start( IEngineSurface *engineSurface, int interfaceVersion ) = 0;
+#ifdef XASH_BUILD_COMMIT
+	virtual void Start( int width, int height ) = 0;
+#else
+	virtual void Start( struct cl_enginefuncs_s *engineFuncs, int interfaceVersion ) = 0;
+#endif
 	virtual void Shutdown() = 0;
 	virtual int Key_Event( int down, int keynum, const char *pszCurrentBinding ) = 0;
 	virtual void CallEngineSurfaceAppHandler( void *event, void *userData ) = 0;
